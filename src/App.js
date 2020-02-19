eb756510ce0c01f01dc27857657ffa421de06a84
sharonnwong <shachi19@uw.edu>
2020-02-19 10:51:48 -0800

    add search and background

--- src/App.js
+++ src/App.js
@@ -1,26 +1,49 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useContext,useState} from 'react';
 import './App.css';
+import {Input, Button} from 'antd'
+
+const context = React.createContext()
 
 function App() {
-  return (
+  const [state, setState] = useState({
+    searchTerm:''
+  })
+  return <context.Provider value={{
+    ...state,
+    set: v=> setState({...state, ...v})
+  }}>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Body />      
     </div>
-  );
+  </context.Provider>
+}
+
+function Body() {
+  const ctx = useContext(context)
+
+  return <header className="App-body">
+    <link href="https://fonts.googleapis.com/css?family=Reenie+Beanie&display=swap" rel="stylesheet"></link>
+    <div className="city">
+      <h1>City</h1>
+    </div>
+    <div className="searching">
+      <Input 
+        className="input"
+        value={ctx.searchTerm}
+        onChange={e=> ctx.set({searchTerm:e.target.value})}
+        placeholder="Search for a city"
+        onKeyPress={e=>{
+          if(e.key==='Enter' && ctx.searchTerm) search(ctx)
+        }}
+      />
+      <Button className="button" shape="circle" icon="search" onClick={()=> search(ctx)} disabled={!ctx.searchTerm}/>
+    </div>
+  </header>
+}
+
+async function search({searchTerm, set}){
+  console.log(searchTerm)
+  set({searchTerm:''})
 }
 
 export default App;
